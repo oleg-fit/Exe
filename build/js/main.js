bf592952e1c0c555b2ca73729cacb4351fb8b519
oleg-fit <oleg26-spb@rambler.ru>
2020-05-09 14:30:47 +0300

    Добавляет разметку и стилизацию блока responsive

--- build/js/main.js
+++ build/js/main.js
@@ -1,11 +1,4 @@
-let itemWorksListBtn = document.querySelector('.works__list-zoom');
-let itemWorksListLink = document.querySelector('.works__list-link');
+// let items = document.querySelectorAll('.works__list-item');
+// let itemWorksListBtn = document.querySelector('.works__list-zoom');
+// let itemWorksListLink = document.querySelector('.works__list-link');
 
-itemWorksListBtn.on('focus', function(event) {
-  var target = event.target;
-
-  if (this.classList.contains('active')) {
-    this.classList.add('.works__list-item--active')
-  }
-
-})
